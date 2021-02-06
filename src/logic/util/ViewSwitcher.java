d8fb5545920509b242b935e34c195bc83bedf04b
Francesco Dalena <57172898+kecco9939@users.noreply.github.com>
2021-02-06 19:27:15 +0100

    added methods on viewSwitcher

--- src/logic/util/ViewSwitcher.java
+++ src/logic/util/ViewSwitcher.java
@@ -1,5 +1,67 @@
 package logic.util;
+import logic.util.enumeration.Views;
+import javafx.fxml.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import logic.util.Session;
+import java.io.IOException;
 
 public class ViewSwitcher {
-
+	private static final String PATH="../view/resources";
+	
+	private ViewSwitcher() {/*Nothing to do here*/}
+	
+	public static FXMLLoader loadFXML(Views view) {
+		switch(view) {
+		
+		case LOGIN:	
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "LogIn.fxml"));
+		case MYACCOUNT:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "MyAccount.fxml"));
+		case MYGROUPS:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "MyGroups.fxml"));
+		case MYRESERVATIONS:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "MyReservation.fxml"));
+		case MYREVIEWS:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "MyReviews.fxml"));
+		case MYROOMS:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "MyRooms.fxml"));
+		case ROOMSEARCH:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "Search.fxml"));
+		case OTHERACCOUNT:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "OtherAccount.fxml"));
+		case POSTROOM:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "PostRoom.fxml"));
+		case REVIEWFORM:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "ReviewForm.fxml"));
+		case GROUPBOOKING:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "GroupBookRoom.fxml"));
+		case GROUPCREATION:
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "CreateGroup.fxml"));
+		default: /*case HOME*/
+			return new FXMLLoader(ViewSwitcher.class.getResource(PATH + "Home.fxml"));
+		}
+	}
+	
+	public static Scene switchTo (Views nextView, Initializable controller) {
+		Session.getSession().setCurrView(nextView);
+	try {
+		if (nextView.equals(Views.LOGIN)) {
+			Session.getSession().setCurrUser(null);
+			return new Scene(loadFXML(nextView).load());
+		}
+		else {
+			FXMLLoader loader = loadFXML(nextView);
+			if (controller != null)
+				loader.setController(controller);
+			return new Scene(loader.load());
+		}
+	}
+	catch (IOException e)
+	{
+		return new Scene(create404Page(nextView.toString().toLowerCase()));
+	}
+	}
+	
+	private static void /*VBox*/ create404Page(String view) {/*INSERIRE UNA VBOX DI ERRORE*/}
 }
