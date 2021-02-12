534f1818e56823dd2ef12975cc0b90023a42da9f
Francesco Dalena <57172898+kecco9939@users.noreply.github.com>
2021-02-12 17:07:11 +0100

    test group controller doesn't work

--- src/test/fd/TestGroupController.java
+++ src/test/fd/TestGroupController.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 import logic.controller.RegistrationController;
 import logic.exception.AccountException;
 import logic.exception.DatabaseException;
-import logic.model.Account;
-import logic.model.Group;
 import logic.model.dao.AccountDAOImpl;
 import logic.model.dao.GroupDAOImpl;
 import logic.model.dao.PersonDAOImpl;
@@ -19,29 +17,23 @@ import static org.junit.Assert.*;
 
 import java.sql.SQLException;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 /*
  * 
  * TESTING BY @author Francesco Dalena
+ * 					  matr. 0266977
  * 
  * */
 public class TestGroupController {
-	GroupBean testGroupBean;
-	Group testGroup;
-	GroupController groupTestCtrl;
-	Account testAccount;
-	PersonBean adminPBean;
-	AccountBean adminABean;
-	int idAdmin;
-	PersonBean partecipantPBean;
-	AccountBean partecipantABean;
-	int idPartecipant;
 	
-	@Before /* Create two accounts to work with */
-	public void init() {
-		adminPBean = new PersonBean("Maciste","Middle School", "Massaia", "WWVVDO87H12H625A",4,4);
-		adminABean = new AccountBean("WWVVDO87H12H625A","Di Nome", "Di Fatto", "maciste@gmail.com", "pincopallo", "1999/01/01",0);
+	static int idAdmin;
+	static int idPartecipant;
+	
+	@BeforeClass /* Create two accounts to work with */
+	public static void init() {
+		PersonBean adminPBean = new PersonBean("Maciste","Middle School", "Massaia", "WWVVDO87H12H625A",4,4);
+		AccountBean adminABean = new AccountBean("WWVVDO87H12H625A","Di Nome", "Di Fatto", "maciste@gmail.com", "pincopallo", "1999/01/01",0);
 		RegistrationController regCtrl = RegistrationController.getInstance();
 		try {
 			regCtrl.register(adminABean, adminPBean);
@@ -49,8 +41,8 @@ public class TestGroupController {
 		} catch (DatabaseException | SQLException e) {
 			e.printStackTrace();
 		}
-		partecipantPBean = new PersonBean("Fragola86","University", "Sapienza", "WWVVDO47H12H625A",3,2);
-		partecipantABean = new AccountBean("WWVVDO47H12H625A","Maria", "Dei Monti", "fragola86@gmail.com", "pincopallo", "1999/01/02",0);
+		PersonBean partecipantPBean = new PersonBean("Fragola86","University", "Sapienza", "WWVVDO47H12H625A",3,2);
+		AccountBean partecipantABean = new AccountBean("WWVVDO47H12H625A","Maria", "Dei Monti", "fragola86@gmail.com", "pincopallo", "1999/01/02",0);
 		try {
 			regCtrl.register(partecipantABean, partecipantPBean);
 			idPartecipant = PersonDAOImpl.getInstance().getPersonId(partecipantPBean);
@@ -59,20 +51,11 @@ public class TestGroupController {
 		}
 	}
 	
-	@After /* Delete the two accounts created*/
-	public void tearDown() {
-		try {
-			AccountDAOImpl.getInstance().removeAccount(adminABean);
-			AccountDAOImpl.getInstance().removeAccount(partecipantABean);
-		} catch (SQLException e) {
-
-			e.printStackTrace();
-		}
-	}
-	
 	@Test /* Make sure a new created group has only one member*/
 	public void testCreateGroupDefaultSize() {
-		groupTestCtrl = GroupController.getInstance();
+		GroupBean testGroupBean = new GroupBean();
+		GroupController groupTestCtrl = GroupController.getInstance();
+		
 		testGroupBean.setAdmin("WWVVDO87H12H625A");
 		testGroupBean.setName("Best Group Ever");
 		int size=0;
@@ -87,6 +70,13 @@ public class TestGroupController {
 	
 	@Test /*Make sure adding a member generate a size increment by 1*/
 	public void testAddGroupPartecipantSize() {
+		GroupBean testGroupBean = new GroupBean();
+		GroupController groupTestCtrl = GroupController.getInstance();
+		PersonBean partecipantPBean = new PersonBean("Fragola86","University", "Sapienza", "WWVVDO47H12H625A",3,2);
+		
+		testGroupBean.setAdmin("WWVVDO87H12H625A");
+		testGroupBean.setName("Best Group Ever");
+		
 		int oldPartecipants=0;
 		int newPartecipants=0;
 		try {
@@ -101,6 +91,11 @@ public class TestGroupController {
 	
 	@Test /*Make sure removing a group does remove it from the database*/
 	public void testDeleteGroup() {
+		GroupBean testGroupBean = new GroupBean();
+		GroupController groupTestCtrl = GroupController.getInstance();
+		testGroupBean.setAdmin("WWVVDO87H12H625A");
+		testGroupBean.setName("Best Group Ever");
+		
 		boolean value = true;
 		try {
 			groupTestCtrl.deleteGroup(testGroupBean);
@@ -112,6 +107,19 @@ public class TestGroupController {
 			e.printStackTrace();
 		}
 		assertEquals(false,value);		
+	}
+	
+	@AfterClass /* Delete the two accounts created*/
+	public static void tearDown() {
+		AccountBean adminABean = new AccountBean("WWVVDO87H12H625A","Di Nome", "Di Fatto", "maciste@gmail.com", "pincopallo", "1999/01/01",0);
+		AccountBean partecipantABean = new AccountBean("WWVVDO47H12H625A","Maria", "Dei Monti", "fragola86@gmail.com", "pincopallo", "1999/01/02",0);
+		try {
+			AccountDAOImpl.getInstance().removeAccount(adminABean);
+			AccountDAOImpl.getInstance().removeAccount(partecipantABean);
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
 	}
 	
 	
